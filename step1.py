b529db3aadcb00a23d8f26a708fdda10d1f26f9e
Tanzim Raha <raha2312091031@diu.edu.bd>
2026-05-23 07:25:51 +0600

    Add files via upload

---
+++ step1.py
@@ -0,0 +1,61 @@
+# WE Learn from this Step
+"""print statement,input,comment,variable,
+operators(Arithmetic,assignment,comparison,logical,membership,bitwise)"""
+
+#How to print
+print("Hello world!")
+print("My name is Tanzim.",end=" ")
+print("I am a student.")
+
+#how to take input 
+#making variable
+"""REMOVE COMMENT"""
+#num=int(input("enter a number:"))   
+#print(num)
+"""REMOVE COMMENT"""
+#name=input("enter your name:")
+#Print("your name:  ",name)
+
+#python variables Data type
+"""In python main data type string,but we can also get int ,float,Boolean,complex,set, dictionary,list tuples,"""
+#examples
+num1=int(12)
+print(type(num1))
+
+num2=float(2.33)
+print(type(num2))
+
+name1='tanzim'
+print(type(name1))
+
+num3 = 2 + 3j
+print(num3)
+print(type(num3))
+
+is_active = True
+print(is_active)
+print(type(is_active))
+
+#convert one data type to another
+
+a = int(3.7)     # 3 (decimal removed)
+b = int("10")    # string → int
+
+print(a, b)
+
+
+x = float(5)
+print(x)
+print(type(x))
+
+y = str(100)     
+print(y)
+print(type(y))
+
+
+print(bool(0))      # False
+print(bool(1))      # True
+print(bool(""))     # False
+print(bool("Hi"))   # True
+
+
